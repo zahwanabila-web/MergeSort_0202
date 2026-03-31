d90df1088aaa05560701cadc74ea9253b2940c85
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-31 10:43:34 +0700

    input

---
+++ MergeSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+        
+    }
+}
